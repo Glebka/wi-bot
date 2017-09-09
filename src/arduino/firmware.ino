09ee9279d7669bf4f174d2ac93e78c506f04b777
Hlieb Romanov <rgebox@gmail.com>
2017-09-09 15:50:34 +0300

    [Arduino] Firmware code refactoring.

--- src/arduino/firmware.ino
+++ src/arduino/firmware.ino
@@ -1,132 +1,87 @@
-#include <Wire.h>
+/*************************************************************************
+ * WiBot Arduino Pro Mini firmware
+ *************************************************************************
+ * @file    firmware.ino
+ * @date
+ * @author  Hlieb Romanov
+ * @brief   Main firmware file for Arduino Pro Mini board
+ ************************************************************************/
 #include <Timer.h>
-#include <Adafruit_PWMServoDriver.h>
+#include <Servo.h>
 
-#define MAX_PWM_SERVO 500
-#define MIN_PWM_SERVO 140
-#define STEP 5
-#define INDICATOR_PIN 9
-#define LEFT_MOTOR_PIN 3
-#define RIGHT_MOTOR_PIN 5
+#include "common.h"
+#include "commands.h"
+#include "promini_pins.h"
 
-Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
-Timer timer;
-int timeoutEvent = 0;
-
-uint16_t rotX = MIN_PWM_SERVO; // up-down servo
-uint16_t rotZ = MIN_PWM_SERVO; // left-right servo
-uint16_t motorL = 0;
-uint16_t motorR = 0;
+Servo servoX;         // servo motor that rotates camera around X axis (up-down)
+Servo servoZ;         // servo motor that rotates camera around Z axis (left-right)
+Timer timer;          // timer for oscillating the status LED and for timeout handling
+int timeoutEvent = 0; // timeout event ID for handling situations when HU ocasionally disconnects
 
 byte isInitialized = 0;
 
-struct Command
-{
-  uint16_t code;
-  uint16_t hiParam;
-  uint16_t loParam;
-};
-
 Command cmd;
 
-void resetMotors( void* context )
+/**
+ * Callback function that turns off motors.
+ * This callback is called when HU doesn't send commands during 500 ms
+ * @param context - not used 
+ */
+void resetMotors(void *context)
 {
-  analogWrite( LEFT_MOTOR_PIN, 0 );
-  analogWrite( RIGHT_MOTOR_PIN, 0 );
+  analogWrite(LEFT_MOTOR_PIN, 0);
+  analogWrite(RIGHT_MOTOR_PIN, 0);
 }
 
-void setup( void ) 
+/**
+ * Setup function for initializing the board's hardware
+ */
+void setup(void)
 {
-  Serial.begin( 115200 );
-  pwm.begin();
-  pwm.setPWMFreq( 60 );  // Analog servos run at ~60 Hz updates
-  pwm.setPWM( 0, 0, rotZ );
-  pwm.setPWM( 1, 0, rotX );
-  analogWrite( LEFT_MOTOR_PIN, 0 );
-  analogWrite( RIGHT_MOTOR_PIN, 0 );
-  //timer.oscillate( INDICATOR_PIN, 100, LOW, 10 );
+  Serial.begin(DEFAULT_BAUD_RATE);
+  servoX.attach(X_AXIS_SERVO_PIN, MIN_PWM_SERVO, MAX_PWM_SERVO);
+  servoZ.attach(Z_AXIS_SERVO_PIN, MIN_PWM_SERVO, MAX_PWM_SERVO);
+  resetMotors(NULL);  
 }
 
-void loop( void ) 
+void loop(void)
 {
-  if (Serial.available() > 0) 
+  if (Serial.available() > 0)
   {
-    if ( Serial.readBytes( ( char* )&cmd, sizeof( Command ) ) == 0 )
+    if (Serial.readBytes((char *)&cmd, sizeof(Command)) == 0)
     {
-      timer.oscillate( INDICATOR_PIN, 50, LOW, 2 );
+      timer.oscillate(INDICATOR_PIN, 50, LOW, 2);
     }
-    
-    if ( !isInitialized )
+
+    if (!isInitialized)
     {
-      if ( cmd.code == 0xFF && cmd.hiParam == 0x1 && cmd.loParam == 0xFF )
+      if (memcmp(&cmd, &INIT_CMD, sizeof(struct Command)) == 0)
       {
         isInitialized = 1;
-        timer.oscillate( INDICATOR_PIN, 100, LOW, 10 );
+        timer.oscillate(INDICATOR_PIN, 100, LOW, 10);
       }
       goto upd;
     }
-    
-    if ( cmd.code == 0xFA ) // servo control
-    {
-      //timer.oscillate( INDICATOR_PIN, 50, LOW, 2 );
-      rotX = cmd.hiParam;      
-      rotZ = cmd.loParam;
-      
-      if ( rotX > MAX_PWM_SERVO )
-      {
-        timer.oscillate( INDICATOR_PIN, 50, LOW, 2 );
-        rotX = MAX_PWM_SERVO;
-      }
-      if ( rotZ > MAX_PWM_SERVO )
-      {
-        timer.oscillate( INDICATOR_PIN, 50, LOW, 2 );
-        rotZ = MAX_PWM_SERVO;
-      }
-      if ( rotX < MIN_PWM_SERVO )
-      {
-        timer.oscillate( INDICATOR_PIN, 50, LOW, 2 );
-        rotX = MIN_PWM_SERVO;
-      }
-      if ( rotZ < MIN_PWM_SERVO )
-      {
-        timer.oscillate( INDICATOR_PIN, 50, LOW, 2 );
-        rotZ = MIN_PWM_SERVO;
-      }
-      pwm.setPWM( 0, 0, rotZ );
-      pwm.setPWM( 1, 0, rotX );
-    }
-    else
-    {
-      if ( cmd.code == 0xFB ) // motor control
-      {
-        timer.stop( timeoutEvent );
-        motorL = cmd.hiParam;
-        motorR = cmd.loParam;
-        analogWrite( LEFT_MOTOR_PIN, motorL );
-        analogWrite( RIGHT_MOTOR_PIN, motorR );
-        timeoutEvent = timer.after( 500, resetMotors, NULL );
-      }
+
+    switch(cmd.code) {
+      case CMD_CODE_CAM_ROTATE:
+        {
+          servoX.write(cmd.hiParam);
+          servoZ.write(cmd.loParam);
+        }
+        break;
+      case CMD_CODE_MOTOR_CONTROL:
+        {
+          timer.stop(timeoutEvent);
+          analogWrite(LEFT_MOTOR_PIN, cmd.hiParam);
+          analogWrite(RIGHT_MOTOR_PIN, cmd.loParam);
+          timeoutEvent = timer.after(NEXT_CMD_TIMEOUT_MS, resetMotors, NULL);
+        }        
+        break;
+      default:
+        break;
     }
   }
-  upd:
+upd:
   timer.update();
-  /*rotX = 140;
-  while ( true )
-  {
-    while( rotX !=500 )
-    {
-      pwm.setPWM( 0, 0, rotX );
-      pwm.setPWM( 1, 0, rotX );
-      delay(10);
-      ++rotX;
-    }
-    while ( rotX != 140 )
-    {
-      pwm.setPWM( 0, 0, rotX );
-      pwm.setPWM( 1, 0, rotX );
-      delay(10);
-      --rotX;
-    }
-  }*/
 }
